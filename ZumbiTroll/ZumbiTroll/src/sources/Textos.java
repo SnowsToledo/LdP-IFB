7b1f77e6b911d3ba83b87800eb37b3653d0bf956
Wanghley <wanghley2012@gmail.com>
2018-04-29 10:05:14 -0300

    Code Update

---
+++ ZumbiTroll/ZumbiTroll/src/sources/Textos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sources;
+
+/**
+ *
+ * @author wanghley
+ */
+public class Textos {
+     String  intro = "Seja bem-vindo ao jogo que as suas decisões podem  "
+                + "matar ou salvar alguém...\nSó depende de você! Boa sorte!"
+             + "Você está pronto?\n 1 - SIM\n  2 - NÃO\n\n";
+     String ini = "Você tomou essa decisão... A partir de agora tudo está por sua\n"
+             + "conta em risco... Salve-se... A aventura começa agora...";
+}
