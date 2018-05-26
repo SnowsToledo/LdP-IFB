7a7c598ccf5304cae24160906bfacb07fc1910b3
Wanghley Soares Martins <wanghley2012@gmail.com>
2018-05-25 22:08:27 -0300

    Correções no Texto

--- ZumbiTroll/ZumbiTroll/src/sources/Textos.java
+++ ZumbiTroll/ZumbiTroll/src/sources/Textos.java
@@ -16,17 +16,21 @@ public class Textos {
              + "............................................................................."
              + "...................................................................................";
      
-     String a1 = "\nEm 2025, nos Estados Unidos, Texas. Ocorreu um acidente químico,\n"
-             + " no qual as as pessoas começam a ficar inconcientes e com fome de carne.\n"
-             + "Você está no seu apartamento e precisa sobreviver a esse apocalipse...\n"
-             +"Você escuta explosões de bombas sendo detonadas... O que você faz:\n\n"
-             + "1. Ir ao terraço verificar o que está acontecendo.\n"
-             + "2. Ficar no apartamento e esperar.\n";
+     String a1 = "\n2025, nos Estados Unidos, Texas...\n"
+             + "Ocorreu um acidente químico, no qual as pessoas\n"
+             + " começam a ficar incocientes e com fome de carne.\n"
+             + "Você está no seu apartamento e precisa sobreviver a esse apocalipse...\n" +""
+             + "Primeiramennte você olha a foto de sua amada esposa, Mary. Sempre quando você ver a foto dela,\n"
+             + " você se inspira, a esperança volta em você...\n"
+             + " Você escuta explosões... O que você faz:"
+             + "        1. Ir ao terraço verificar o que está acontecendo\n"
+             + "        2. Ficar no apartamento e esperar.";
      //seleciona 1 em a1
      String b1 = "\n                                        -----------------------\n"
              + "                                        | FIM DE JOGO |\n"
              + "                                        -----------------------\n"
-             + "Você morre, com estilhaços de vidro derivados da explosão\n";
+             + "Você morre, com estilhaços de vidro derivados da explosão,\n"
+             + "e você vê, na sua mente, sua esposa decepicionada";
      //seleciona 2 em a1
      String b2 = "\nVocê fica no seu apartamento no 14° andar(último andar), "
              + "mas escuta barulhos no 7° andar e vai verificar...\n"
@@ -59,7 +63,7 @@ public class Textos {
      String c3 = "\nOs zumbis estranhamente não percebem a sua presença e você consegue"
              + "fugir...\n";
      String d = "No fim do corredor há um telefone . Sua descisão é:\n"
-             + "\n1-Você liga pro exército pra chamar um resgate e espera.\n"
+             + "\n1-Você liga pro exército.\n"
              + "2-Você desce no térreo e tenta sobreviver por si só.\n";
      String d1 = "\nAgora você deve sobreviver até a chegada do resgate. \n"
              + "No caminho da volta você encontra um garoto sendo atacado por um zumbi. O que você faz:\n"
@@ -75,16 +79,84 @@ public class Textos {
              + "                                        -----------------------\n"
              + "Você consegue deter esses andarilhos e salva o garoto...\n"
              + "O garoto pede para que ele te acompanhe até a chegada do resgate"
-             + "e você aceita, sem pensar duas vezes, pois tens um fraco por crianças...\n"
+             + "e você aceita, sem pensar duas vezes, pois tens um fraco por crianças...\nBeth amava crianças......\n"
              + "Porém, tempos depois ele começa a se transformar e você não percebe...\n"
              + "ELE TE MORDE E VOCÊ TAMBÉM É INFECTADO!\nVocê não é bom o suficiente para esse jogo\n!";
      String e2 = "\nVocê fica com pena do garoto, mas a sua vida é mais importante nos dias de hoje...\n"
              + "Você continua vivo por enquanto...\n Você decide ir ao terraço...\n"
-             + "Chegando lá, você espera muito tempo e nada do resgate... Você começa a ficar com fome...\n"
-             + "O que você faz?\n"
-             + "1.\n"
-             + "2.\n";
-     String fim = "\"\\n----------------------------------------------------------\\n\"\n" +
-"             + \"                                        | FIM DE JOGO |\\n\"\n" +
-"             + \"----------------------------------------------------------\\n\"";
+             + "Chegando lá você espera muito até um ponto em que você fica com fome. O que fazer:\n"
+             + "        1.Você vai atrás de comida\n"
+             + "        2.Você espera por mais um tempo o resgate";
+     String f1 = "Você vai até o andar de baixo procurar comida.\n"
+             + " Você vai entrado de porta em porta até que você acha uma casa com ingredientes\n"
+             + " suficientes para fazer uma boa refeição.E esses ingredientes são:\n"
+             + "    -Macarrao espaguete,arroz,feijão,carne,molho branco,ovo,sal;\n"
+             + "Dessas podem se fazer: arroz e feijão com carne,macarrão com ovo mexido e macarrão à molho branco.\n"
+             + "Qual delas você escolhe(lembrando que você tem que ser rapido então escolha uma que seja fácil e rápida):\n"
+             + "        1.arroz e feijão com carne;\n"
+             + "        2.macarrão com ovo mexido;\n"
+             + "        3.macarrão à molho branco;\n";
+     String f1_1 = "\n                                        -----------------------\n"
+             + "                                        | FIM DE JOGO |\n"
+             + "                                        -----------------------\n" 
+             +"\nVocê espera muito para terminar a refeição.O resgate aperece e você perde ele.\n";
+     String f1_3 = "\n                                        -----------------------\n"
+             + "                                        | FIM DE JOGO |\n"
+             + "                                        -----------------------\n" 
+             +"\nEssa é a unica que você não sabe fazer, mas sabe que é possivel.\n";
+     String f1_2 = "uma boa escolha. Você faz rapidamente e e se satisfaz.\n"
+             + "Você começa a escutar barulhos... Você percebe que é o resgate, um helicóptero. Eles jogam uma escada de corda.\n"
+             + "Você começa a subir a escada, a foto da sua amada ameaça a cair então você deve escolher:\n"
+             + "        1.Pegar a foto.\n"
+             + "        2.Coninuar e deixar a foto cair.\n";
+     String f1_2_1 = "\n                                        -----------------------\n"
+             + "                                        | FIM DE JOGO |\n"
+             + "                                        -----------------------\n" 
+             +"Você perde o equilibrio e cai da escada e não adinata nada ter a foto.\n";
+     String g = "Voce continua subindo mais rápido e para sua sorte você havia uma outra foto em sua carteira...\n"
+             + "Ao olhar para o alto observa que os tripulantes do helicóptero começaram a devorar um ao outro e você resolve:\n"
+             + "        1. Continuar subindo e tomar o controle.\n"
+             + "        2. Descer a escada muito rápido.\n";
+     String g1 = "\n                                        -----------------------\n"
+             + "                                        | FIM DE JOGO |\n"
+             + "                                        -----------------------\n" 
+             +"Você é mordido por um dos tripulantes ao apoiar a mão no assoalho do helicóptero!\n";
+     String g2 = "\nVocê consegue escapar dessa vez...\n"
+             + "Está começando à anoitecer e você precisa arrumar um lgar seguro para dormir.\n"
+             + "O que você faz:\n"
+             + "        1.Voltar para o apartamento em que você estava.\n"
+             + "        2.procurar com cautela outro apartamento.\n"
+             + "        3.Ficar no terraço pois é o lugar mais seguro.\n";
+     String g2_1 = "\n                                        -----------------------\n"
+             + "                                        | FIM DE JOGO |\n"
+             + "                                        -----------------------\n" 
+             +"Você até que conseguiu dormir, mas por você já ter estado lá os zumbis gravaram seu cheiro e te cercaram.\n";
+     String g2_2 = "\n                                        -----------------------\n"
+             + "                                        | FIM DE JOGO |\n"
+             + "                                        -----------------------\n"
+             + "Por incrivel que pareça só existia um lugar seguro...\n e era o que você estava.\n";
+     String h = "Parabéns! Você achou o lugar perfeito, pois você sempre precisa ter\n"
+             + " cuidado a noite... A qualquer hora pode aparecer um zumbi para lhe saborear...\n"
+             + "Novamente você precisa pedir reforços...\nO que fazer:\n"
+             + "        1.Tentar sobreviver o máximo que puder até o seu fim.\n"
+             + "        2.Voltar para a metade do predio para usar o telefone de emergência.\n";
+     String h1 = "\n                                        -----------------------\n"
+             + "                                        | FIM DE JOGO |\n"
+             + "                                        -----------------------\n"  
+             +"Você não aguenta por muito tempo, com isso você falhou e esse não era o seu objetivo.\n";
+     String i = "\nVocê continua no seu trajeto.Boa sorte.\n" +
+"	     descendo as escadas vc percebe que tem um monte de zumbis,e só falta um andar\n"
+             + " até vc chegar no telefone.O que fazer:\n"
+             + "        1.Jogar algum objeto para chamar atenção deles.\n" +
+"       2.Jogar seu perfume na direção aposta a sua.\n";
+     String i1 =  "\n                                        -----------------------\n"
+             + "                                        | FIM DE JOGO |\n"
+             + "                                        -----------------------\n" 
+             +"Você não espescificou o lugar em que você iria jogar\n"
+             + " e acabou chamando a atenção deles para você.\n";
+     String j = "\nVocê consegue o que quer...\n\n\n" +
+"	     Espera!!!\nUm zumbi te seguiu, o que você vai fazer:\n" +
+"       1.Emboscar e matar ele.\n" +
+"	     2.Descer mais andares para tentar dispistar eles.";
+     
 }
